aa784668bc6f225ac9337fdee4dc0eac2e953f3f
womack <womackx@live.com>
2019-06-06 11:37:30 +0200

    fetches/stores to s3

--- hello.py
+++ hello.py
@@ -1,19 +1,39 @@
 import json
+import boto3, botocore
 from flask import Flask,  request, g, jsonify
-app = Flask(__name__)
+from config import S3_KEY, S3_SECRET, S3_BUCKET
 
-cachedData = []
+s3 = boto3.resource(
+   "s3",
+   aws_access_key_id=S3_KEY,
+   aws_secret_access_key=S3_SECRET
+)
+
+app = Flask(__name__)
+app.config.from_object("config")
 
 @app.route("/", methods=["GET"])
 def getData():
-    return jsonify(cachedData)
+    obj = s3.Object("innovation-arts", "data.json")
+    body = obj.get()['Body'].read()
+    return body
 
 @app.route("/", methods=["POST"])
 def appendData():
-    cachedData.append(request.json)
-    return "appended stuff"
+    if(hasattr(request,"data"))
+        obj = s3.Object("innovation-arts", "data.json")
+        body = obj.get()['Body'].read()
+        body = body.decode("utf8").replace("'", '"')
+        body = json.loads(body)
+        body.append(request.json.get("data"))
+        with open("temporary_file","wb") as fo:
+            fo.write(json.dumps(body).encode("utf-8"))
+        s3.meta.client.upload_file(
+                        "./temporary_file",
+                        "innovation-arts",
+                        "data.json"
+                    )
+        return "updated"
     
-
-
 if __name__ == "__main__":
     app.run()
